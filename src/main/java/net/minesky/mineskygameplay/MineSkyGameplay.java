39a61dd1ba2f40a47f4670c6d01bbeae14550ba9
drawned <drawnwylde@gmail.com>
2026-09-14 17:56:00 -0300

    feat: add join-quit-message

--- src/main/java/net/minesky/mineskygameplay/MineSkyGameplay.java
+++ src/main/java/net/minesky/mineskygameplay/MineSkyGameplay.java
@@ -2,17 +2,23 @@ package net.minesky.mineskygameplay;
 
 import io.papermc.paper.event.player.PlayerDeepSleepEvent;
 import io.papermc.paper.event.server.ServerResourcesReloadedEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minesky.mineskygameplay.advancements.AdvancementsAPI;
 import net.minesky.mineskygameplay.advancements.command.ConquistasCommand;
 import net.minesky.mineskygameplay.advancements.hook.MythicHook;
 import net.minesky.mineskygameplay.advancements.loader.DynamicAdvancementLoader;
 import net.minesky.mineskygameplay.advancements.menu.BedrockMenuManager;
 import org.bukkit.WeatherType;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MineSkyGameplay extends JavaPlugin implements Listener {
@@ -21,8 +27,19 @@ public final class MineSkyGameplay extends JavaPlugin implements Listener {
     private BedrockMenuManager menuManager;
     private DynamicAdvancementLoader loader;
 
+    private boolean disableChat = true;
+    private boolean disableJoinQuit = true;
+
+    private FileConfiguration config;
+
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
+
+        config = this.getConfig();
+        disableChat = config.getBoolean("disable-chat", true);
+        disableJoinQuit = config.getBoolean("disable-join-quit", true);
+
         this.api = new AdvancementsAPI(this);
         this.menuManager = new BedrockMenuManager(this);
         this.loader = new DynamicAdvancementLoader(this, api);
@@ -41,7 +58,15 @@ public final class MineSkyGameplay extends JavaPlugin implements Listener {
     }
 
     @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        if(disableJoinQuit)
+            e.quitMessage(null);
+    }
+    @EventHandler
     public void onJoin(PlayerJoinEvent e) {
+        if(disableJoinQuit)
+            e.joinMessage(null);
+
         final Player p = e.getPlayer();
 
         if(p.hasPlayedBefore()) {
@@ -53,6 +78,20 @@ public final class MineSkyGameplay extends JavaPlugin implements Listener {
 
         p.setPlayerWeather(WeatherType.CLEAR);
         p.setPlayerTime(0, false);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent e) {
+        if(!disableChat) return;
+
+        if(e.isCancelled())
+            return;
+        e.setCancelled(true);
+
+        e.getPlayer().sendMessage(
+                Component.text("O chat está desligado por enquanto, aguarde.")
+                        .color(NamedTextColor.RED)
+        );
     }
 
     @EventHandler
